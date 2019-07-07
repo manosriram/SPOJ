8505a215dd3f48c0515c1dd0f092acc4567102ca
Mano Sriram <mano.sriram0@gmail.com>
2019-07-07 13:38:03 +0530

    Add Template.

---
+++ Template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#define trace(x) cerr << #x << " " << x << '\n';
+#define ld long long
+#define disp(n) for (auto t : n)
+#define dispR(t, n1, n2)
+#define pb push_back
+#define mod 1000000007
+#define c_sort(a) sort(a, a + sizeof(a) / sizeof(a[0]))
+#define NL '\n'
+#define INF (int)1e9
+using namespace std;
+typedef vector<int> vI;
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
